4af0dcd9adbe7acc6ea11f94d40d752bb5051a69
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2022-09-24 10:57:58 -0700

    Set up basic react/express

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <p className="text-3xl">TEST</p>
+    </div>
+  );
+}
+
+export default App;
--- test.py
+++
@@ -1,6 +0,0 @@
-print("hello world")
-print("Bryan")
-print("arhum-2")
-
-def name(n):
-    print(n)
